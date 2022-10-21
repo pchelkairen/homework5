5d87b6d2b08d4e87a45d1a3cfffc579281d15254
Irina <pamir.08@mail.ru>
2022-10-21 16:12:05 +0300

    ex3

---
+++ exercises/ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Поиск суммы элементов массива
+int[] arr = {1,4,8,2};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i] } ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray (arr);
+
+int sum = arr.Sum();
+Console.WriteLine($"Sum: {sum}");
+
