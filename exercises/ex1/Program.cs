a66a0ea22d6ac35e0b2c2b393f0b6b10d2fef23c
Irina <pamir.08@mail.ru>
2022-10-21 16:06:00 +0300

    ex1

---
+++ exercises/ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Поиск минимума
+int[] arr = {1,4,8,2};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i] } ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray (arr);
+
+int min = arr.Min();
+Console.WriteLine($"Min: {min}");
